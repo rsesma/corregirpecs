696d268b37ca2fd16ad70443544bde7224b16f04
drslump <drslump@drslump-H110M-S2H>
2018-07-05 19:19:02 +0200

    v2018.07.05 (tarde): trabajar sobre carpeta en vez de sobre archivos

--- src/corregirpecs/CorregirController.java
+++ src/corregirpecs/CorregirController.java
@@ -26,54 +26,88 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 
 public class CorregirController implements Initializable {
 
     @FXML
-    private TextField zip;
-    @FXML
-    private TextField odt;
+    private TextField dir;
     @FXML
     private CheckBox presencials;
     @FXML
     private CheckBox overwrite;
 
+    //private final String C_DEFDIR = System.getProperty("user.home");
+    //private final String C_DEFDIR = "/Users/r/Desktop/CorregirPECs/2017-18_PEC4_DE0";
+    private final String C_DEFDIR = "/home/drslump/Escritorio/CorregirPECs/2017-18_PEC4_DE0";
     
     @FXML
-    void getZip(ActionEvent event) {
-    	FileChooser chooser = new FileChooser();
-    	chooser.setTitle("Obrir arxiu comprimit");
-    	//chooser.setInitialDirectory(new File(System.getProperty("user.home")));
-    	chooser.setInitialDirectory(new File("/Users/r/Desktop/CorregirPECs/2017-18_PEC4_DE0"));
-    	chooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Arxiu comprimit", "*.zip"));
-    	File f = chooser.showOpenDialog(null);
-        if (f != null) zip.setText(f.getAbsolutePath());
-        else zip.setText("");    	
-    }
-    
-    @FXML
-    void getODT(ActionEvent event) {
-    	FileChooser chooser = new FileChooser();
-    	chooser.setTitle("Obrir plantilla");
-    	//chooser.setInitialDirectory(new File(System.getProperty("user.home")));
-    	chooser.setInitialDirectory(new File("/Users/r/Desktop/CorregirPECs/2017-18_PEC4_DE0"));
-    	chooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("LibreOffice Writer", "*.odt"));
-    	File f = chooser.showOpenDialog(null);
-        if (f != null) odt.setText(f.getAbsolutePath());
-        else odt.setText("");    	
+    void getDir(ActionEvent event) {
+        final DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setTitle("Seleccionar carpeta de treball");
+        directoryChooser.setInitialDirectory(new File(C_DEFDIR));
+        File folder = directoryChooser.showDialog(null);
+        if (folder != null) dir.setText(folder.getAbsolutePath());
+        else dir.setText("");
     }
 
     @FXML
-    void Analisi(ActionEvent event) {
+    void Descomprimir(ActionEvent event) {
+    	File folder;
     	Boolean lContinue = false;
+    	if (dir.getText().isEmpty()) ShowAlert("Indicar la carpeta de treball","Error",AlertType.ERROR);
+    	else lContinue = true;
+    	if (lContinue) {
+    		folder = new File(dir.getText(),"PDFs");
+    		// AQUÍ ME QUEDO: DETECTAR ARCHIVO ZIP
+    	}
+/*        try {
+        	// la carpeta es crea nova; si ja existeix, s'elimina
+        	if (folder.exists()) FileUtils.deleteDirectory(folder); 
+        	folder.mkdir();
+        	
+        	byte[] buffer = new byte[1024];
+            // get the zip file content
+            ZipInputStream zis = new ZipInputStream(new FileInputStream(f));
+            //get the zipped file list entry
+            ZipEntry ze = zis.getNextEntry();
+            while (ze!=null) {
+                String fileName = ze.getName();
+                File newFile = new File(dir + File.separator + fileName);
+
+                // create all non exists folders else you will hit FileNotFoundException for compressed folder
+                new File(newFile.getParent()).mkdirs();
+
+                FileOutputStream fos = new FileOutputStream(newFile);             
+                int len;
+                while ((len = zis.read(buffer)) > 0) {
+                    fos.write(buffer, 0, len);
+                }
+                fos.close();
+                
+                ze = zis.getNextEntry();
+            }
+            zis.closeEntry();
+            zis.close();
+            
+            return true;
+        } catch (Exception e) {
+        	ShowAlert(e.getMessage(),"Error",AlertType.ERROR);
+    		return false;    		
+        }*/
+    }
+    
+    @FXML
+    void Analitzar(ActionEvent event) {
+/*    	Boolean lContinue = false;
     	if (zip.getText().isEmpty()) ShowAlert("Indicar l'arxiu ZIP","Error",AlertType.ERROR);
-    	else if (odt.getText().isEmpty()) ShowAlert("Indicar l'arxiu ODT","Error",AlertType.ERROR);
+    	else if (sol.getText().isEmpty()) ShowAlert("Indicar l'arxiu Solució (TXT)","Error",AlertType.ERROR);
     	else lContinue = true;
     	    	
     	if (lContinue) {
 	    	File fzip = new File(zip.getText());
-	    	File fodt = new File(odt.getText());
+	    	File fsol = new File(sol.getText());
 	    	
 	        if (fzip.isFile()) {
 	        	File dir = new File(fzip.getParent(), "PDFs");
@@ -116,7 +150,7 @@ public class CorregirController implements Initializable {
 	        } else {
 	        	ShowAlert("L'arxiu ZIP no existeix","Error",AlertType.ERROR);
 	        }
-    	}
+    	}*/
     }
         
     public boolean Descomprimeix(File f, File dir) {
@@ -162,105 +196,6 @@ public class CorregirController implements Initializable {
     	}
     }
     
-    public void GeneraSolucio() {
-/*    	try {
-            XComponentContext xLocalContext = Bootstrap.createInitialComponentContext(null);
-            System.out.println("xLocalContext");
-
-            XMultiComponentFactory xLocalServiceManager = xLocalContext.getServiceManager();
-            System.out.println("xLocalServiceManager");
-
-            Object urlResolver = xLocalServiceManager.createInstanceWithContext(
-                    "com.sun.star.bridge.UnoUrlResolver", xLocalContext);
-            System.out.println("urlResolver");
-
-            XUnoUrlResolver xUrlResolver =
-                (XUnoUrlResolver) UnoRuntime.queryInterface(XUnoUrlResolver.class, urlResolver);            
-            System.out.println("xUrlResolve");
-
-            try {
-                String uno = "uno:" + unoMode + ",host=" + unoHost + ",port=" + unoPort + ";" + unoProtocol + ";" + unoObjectName;
-                Object rInitialObject = xUrlResolver.resolve(uno);
-                System.out.println("rInitialObject");
-
-                if (null != rInitialObject) {
-                    XMultiComponentFactory xOfficeFactory = (XMultiComponentFactory) UnoRuntime.queryInterface(
-                        XMultiComponentFactory.class, rInitialObject);
-                    System.out.println("xOfficeFactory");
-
-                    Object desktop = xOfficeFactory.createInstanceWithContext("com.sun.star.frame.Desktop", xLocalContext);
-                    System.out.println("desktop");
-
-                    XComponentLoader xComponentLoader = (XComponentLoader)UnoRuntime.queryInterface(
-                        XComponentLoader.class, desktop);
-                    System.out.println("xComponentLoader");
-                    
-                    PropertyValue[] loadProps = new PropertyValue[3];
-
-                    loadProps[0] = new PropertyValue();
-                    loadProps[0].Name = "Hidden";
-                    loadProps[0].Value = Boolean.TRUE;
-
-                    loadProps[1] = new PropertyValue();
-                    loadProps[1].Name = "ReadOnly";
-                    loadProps[1].Value = Boolean.FALSE;
-
-                    loadProps[2] = new PropertyValue();
-                    loadProps[2].Name = "MacroExecutionMode";
-                    loadProps[2].Value = new Short(com.sun.star.document.MacroExecMode.ALWAYS_EXECUTE_NO_WARN);
-                    
-                    try {
-                        XComponent xComponent = xComponentLoader.loadComponentFromURL("file:///" + inputFile, "_blank", 0, loadProps);              
-                        System.out.println("xComponent from " + inputFile);
-
-                        String macroName = "Standard.Module1.MYMACRONAME?language=Basic&location=application";
-                        Object[] aParams = null;
-
-                        XScriptProviderSupplier xScriptPS = (XScriptProviderSupplier) UnoRuntime.queryInterface(XScriptProviderSupplier.class, xComponent);
-                        XScriptProvider xScriptProvider = xScriptPS.getScriptProvider(); 
-                        XScript xScript = xScriptProvider.getScript("vnd.sun.star.script:"+macroName); 
-
-                        short[][] aOutParamIndex = new short[1][1]; 
-                        Object[][] aOutParam = new Object[1][1];
-
-                        @SuppressWarnings("unused")
-                        Object result = xScript.invoke(aParams, aOutParamIndex, aOutParam);
-                        System.out.println("xScript invoke macro" + macroName);
-
-                        XStorable xStore = (XStorable)UnoRuntime.queryInterface(XStorable.class, xComponent);
-                        System.out.println("xStore");
-
-                        if (outputFileType.equalsIgnoreCase("pdf")) {
-                            System.out.println("writer_pdf_Export");
-                            loadProps[0].Name = "FilterName";
-                            loadProps[0].Value = "writer_pdf_Export";
-                        }
-                        xStore.storeToURL("file:///" + outputFile, loadProps);
-                        System.out.println("storeToURL to file " + outputFile);
-                        
-                        xComponent.dispose();
-
-                        xComponentLoader = null;
-                        rInitialObject = null;
-
-                        System.out.println("done.");
-
-                        System.exit(0);
-
-                    } catch(IllegalArgumentException e) {
-                        System.err.println("Error: Can't load component from url " + inputFile);
-                    }
-                } else {
-                    System.err.println("Error: Unknown initial object name at server side");
-                }           
-            } catch(NoConnectException e) {
-                System.err.println("Error: Server Connection refused: check server is listening...");           }
-        } catch(java.lang.Exception e) {
-            System.err.println("Error: Java exception:");
-            e.printStackTrace();
-        }*/
-    }
-    
     public void ShowAlert(String message, String title, AlertType type) {
     	Alert alert = new Alert(type);
         alert.setTitle(title);
@@ -272,6 +207,6 @@ public class CorregirController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    	
+    	dir.setText(C_DEFDIR);
     }
 }
